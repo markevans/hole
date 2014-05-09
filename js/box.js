77b9b5873ee79599458a3a3bf41edfb5f0ef8ce5
Mark Evans <mark@new-bamboo.co.uk>
2014-05-09 17:32:28 +0100

    sink n hole

--- js/box.js
+++ js/box.js
@@ -24,38 +24,78 @@ window.Box = (function () {
       Matter.Bodies.rectangle(width+offset, height*0.5, 50.5, height+0.5, { isStatic: true }),
       Matter.Bodies.rectangle(-offset, height*0.5, 50.5, height+0.5, { isStatic: true })
     ])
+    // Hole
+    Matter.World.add(world, Matter.Bodies.circle(width/2, height/2, 80, {isStatic: true, render: {fillStyle: 'black'}, collisionLabel: 'hole'}))
+    // Sinks
+    var positions = [
+      [width*1/4, height*1/4],
+      [width*1/4, height*3/4],
+      [width*3/4, height*1/4],
+      [width*3/4, height*3/4]
+    ]
+    positions.forEach(function (pos) {
+      Matter.World.add(world, Matter.Bodies.circle(pos[0], pos[1], 40, {
+        isStatic: true,
+        render: {
+          fillStyle: 'white',
+          strokeStyle: 'black'
+        },
+        collisionLabel: 'sink'
+      }))
+    })
 
     // Init Balls
-    this.addRandomBall('top')
+    for(var i=1; i<8; i++) {
+      this.addRandomBall()
+    }
 
     // Collisions
     var self = this
     Matter.Events.on(this.engine, 'collisionStart', function (event) {
       event.pairs.forEach(function (pair) {
         var a = pair.bodyA,
-            b = pair.bodyB
-        if (a.label === 'Circle Body' &&
-              b.label === 'Circle Body' &&
-              a.render.fillStyle === b.render.fillStyle) {
-          var position = averagePosition(a.position, b.position)
-          self.removeBall(a)
-          self.removeBall(b)
-          self.addBall(combinedRadius(a.circleRadius, b.circleRadius), a.render.fillStyle, position.x, position.y)
+            b = pair.bodyB,
+            objects
+        if ((objects = matchPair(a, b, /ball/, /ball/)) && a.collisionLabel == b.collisionLabel) {
+          Box.COLLISION_HANDLERS.sameColouredBall(self, objects[0], objects[1])
+        } else if (objects = matchPair(a, b, /ball/, 'hole')) {
+          Box.COLLISION_HANDLERS['ball-hole'](self, objects[0], objects[1])
+        } else if (objects = matchPair(a, b, /ball/, 'sink')) {
+          Box.COLLISION_HANDLERS['ball-sink'](self, objects[0], objects[1])
         }
       })
     })
 
   }
 
-  Box.COLOURS = ['red', 'yellow', 'blue', 'green', 'orange', 'black']
+  Box.COLOURS = ['red', 'yellow', 'blue', 'green', 'orange']
+
+  Box.COLLISION_HANDLERS = {
+    sameColouredBall: function (box, a, b) {
+      var position = averagePosition(a.position, b.position)
+      box.removeBall(a)
+      box.removeBall(b)
+      box.addBall(combinedRadius(a.circleRadius, b.circleRadius), a.render.fillStyle, position.x, position.y)
+    },
+
+    'ball-hole': function (box, ball, hole) {
+      box.removeBall(ball)
+    },
+
+    'ball-sink': function (box, ball, sink) {
+      box.addRandomBall()
+    }
+  }
 
   Box.prototype = {
     addBall: function (radius, colour, x, y) {
       var ball = Matter.Bodies.circle(x, y, radius, {
-        restitution: 0.2, render: {
+        restitution: 0.4,
+        render: {
           fillStyle: colour,
           strokeStyle: colour
-        }
+        },
+        collisionLabel: 'ball-'+colour
       })
       Matter.World.add(this.engine.world, ball)
     },
@@ -65,6 +105,7 @@ window.Box = (function () {
     },
 
     addRandomBall: function (side, options) {
+      if(!side) { side = sample(['top', 'bottom', 'left', 'right']) }
       if(!options) { options = {} }
       var radius = options.radius || 30,
           colour = options.colour || sample(Box.COLOURS),
@@ -119,6 +160,15 @@ window.Box = (function () {
 
   function sample (array) {
     return array[Math.floor(Math.random()*array.length)]
+  }
+
+  function matchPair (obj1, obj2, matcher1, matcher2) {
+    if (!obj1.collisionLabel || !obj2.collisionLabel) { return }
+    if (obj1.collisionLabel.match(matcher1) && obj2.collisionLabel.match(matcher2)) {
+      return [obj1, obj2]
+    } else if (obj2.collisionLabel.match(matcher1) && obj1.collisionLabel.match(matcher2)) {
+      return [obj2, obj1]
+    }
   }
 
 })()
